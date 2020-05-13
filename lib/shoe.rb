54a63c0e90f0f242b05e7995936cc2ac3e9aa3ea
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-13 14:48:13 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,2 +1,17 @@
 # Make your shoe class here!
 
+class Shoe 
+  
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+  
+  def initialize(new_brand)
+    @brand = new_brand
+  end
+  
+  def cobble
+    @condition = "New"
+    puts "The shoe has been repaired"
+  end
+  
+end
